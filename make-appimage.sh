30663f0fef572fa068238abb6105e31d2ce90194
fiftydinar <65243233+fiftydinar@users.noreply.github.com>
2025-12-24 12:36:28 +0100

    Update Graphs script for AppImage compatibility
    Modify Graphs script to use environment variable for paths.

--- make-appimage.sh
+++ make-appimage.sh
@@ -23,7 +23,7 @@ quick-sharun /usr/bin/graphs \
              /usr/lib/libgirepository*
 
 # Patch Graphs to use AppImage's directory
-sed -i '/if __name__ == "__main__":/a \
+sed -i '/    import gi/a\
     SHARUN_DIR = os.getenv('"'"'SHARUN_DIR'"'"')' ./AppDir/bin/graphs
 sed -i 's|graph_path_dir = "/usr/share/graphs"|graph_path_dir = os.path.join(SHARUN_DIR, '"'"'share'"'"', '"'"'graphs'"'"')|' ./AppDir/bin/graphs
 sed -i 's|localedir = "/usr/share/locale"|localedir = os.path.join(SHARUN_DIR, '"'"'share'"'"', '"'"'locale'"'"')|' ./AppDir/bin/graphs
