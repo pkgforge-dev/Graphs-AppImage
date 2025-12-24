8516e5f4d2e9afc4ab66b45531f713412d22990e
fiftydinar <65243233+fiftydinar@users.noreply.github.com>
2025-12-24 12:45:01 +0100

    Add DEBLOAT_LOCALE environment variable

--- make-appimage.sh
+++ make-appimage.sh
@@ -15,6 +15,7 @@ export DEPLOY_GTK=1
 export GTK_DIR=gtk-4.0
 export ANYLINUX_LIB=1
 export DEPLOY_LOCALE=1
+export DEBLOAT_LOCALE=0
 export STARTUPWMCLASS=se.sjoerd.Graphs # Default to Wayland's wmclass. For X11, GTK_CLASS_FIX will force the wmclass to be the Wayland one.
 export GTK_CLASS_FIX=1
 
